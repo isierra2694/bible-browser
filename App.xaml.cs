4975e962f0e298e9221c9b89c915725e99178ef5
Indy Sierra <isierra2694@gmail.com>
2023-05-07 11:06:54 -0700

    Initial

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BibleBrowser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void BibleBrowserStartup(object sender, StartupEventArgs e)
+        {
+            Window mainWindow = new BibleBrowserMain();
+            mainWindow.Show();
+        }
+    }
+}
