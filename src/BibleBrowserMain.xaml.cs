e54c69fb46d89ff0258f2235b81b8d05b1e3d95b
Indy Sierra <78444910+isierra2694@users.noreply.github.com>
2023-05-10 17:47:49 -0700

    got bible text displaying

--- src/BibleBrowserMain.xaml.cs
+++ src/BibleBrowserMain.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,36 @@ namespace BibleBrowser
     /// </summary>
     public partial class BibleBrowserMain : Window
     {
+        private List<string> bible = new List<string>();
+
         public BibleBrowserMain()
         {
             InitializeComponent();
+
+            DisplayBible();
+        }
+
+        private void DisplayBible()
+        {
+            VirtualizingPanel.SetIsVirtualizing(BibleDocument, true);
+            LoadBibleFile();
+        }
+
+        private async void LoadBibleFile()
+        {
+            try
+            {
+                var reader = new StreamReader(@"c:\users\isier\downloads\bible.txt");
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    BibleDocument.Items.Add(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
