b573dc1e029ea3a5d7e6337e1064f57dd3f7a6bd
Indy Sierra <isierra2694@gmail.com>
2023-05-31 11:07:09 -0700

    sync

--- obj/Debug/net6.0-windows/App.g.i.cs
+++ obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace BibleBrowser {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace BibleBrowser {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/BibleBrowser;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BibleBrowser;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace BibleBrowser {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public static void Main() {
             BibleBrowser.App app = new BibleBrowser.App();
             app.InitializeComponent();
